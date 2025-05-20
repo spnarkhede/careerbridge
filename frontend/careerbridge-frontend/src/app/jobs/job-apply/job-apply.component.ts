ef5353f7a7442f1348b87f0a1cfe5396e8bab83d
Shubham Narkhede <shubham.narkhede@de.bosch.com>
2025-05-20 11:29:30 +0200

    Folder structure and component creation Second Commit

---
+++ frontend/careerbridge-frontend/src/app/jobs/job-apply/job-apply.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-apply',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-apply.component.html',
+  styleUrl: './job-apply.component.scss'
+})
+export class JobApplyComponent {
+
+}
---
+++ frontend/careerbridge-frontend/src/app/jobs/job-detail/job-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-detail.component.html',
+  styleUrl: './job-detail.component.scss'
+})
+export class JobDetailComponent {
+
+}
