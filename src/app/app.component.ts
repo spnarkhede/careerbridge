704fd3e8f16e664da9a6eb2023b46183b7e7fe07
Shubham Narkhede <65786868+spnarkhede@users.noreply.github.com>
2025-05-22 09:15:11 +0200

    Update global styles and theme

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, SharedModule]
+})
+export class AppComponent {
+  title = 'CareerBridge';
+}
